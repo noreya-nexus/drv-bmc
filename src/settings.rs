1b7ad80eb081fd6195b3dc2d58b96822e9f6645d
Richard Schleich <rs@noreya.tech>
2023-05-30 18:13:36 +0200

    Updated to version 1.0.0.

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,5 @@
 pub const MODULE_NAME : &str = "modules.noreya.tech/bmc";
 pub const DRV_NAME : &str = "nexus-drv-bmc";
 pub const SOCKET_PATH : &str = "/run/nexus-drv-bmc/nexus-drv-bmc.socket";
-
+pub const COMPATIBLE_FW_MAJOR : u16 = 1;
+pub const COMPATIBLE_FW_MINOR : u16 = 0;
